00e47e00a8dfd7e68617481c36efa00c57d53a40
Neimerc <Neil_Mercury@yahoo.es>
2021-02-21 16:31:45 +0100

    Crear un usuario en nuestra Base de Datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,18 +5,29 @@
 // Realmente no hace falta, pero se vuelve a requerir para la ayuda de Intellisense en
 // VSCode
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
 // De nuevo, res = response se hace para obtener la ayuda de Intellisense en VSCode
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
+const crearUsuario = async (req, res = response) => {
+  // const { name, email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'registro',
-    name,
-    email,
-    password,
-  });
+  try {
+    const usuario = new Usuario(req.body);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'registro',
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador',
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
