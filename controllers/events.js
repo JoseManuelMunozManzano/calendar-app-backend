42679b85bc26369df307e93324a9b887697c0054
Neimerc <Neil_Mercury@yahoo.es>
2021-02-23 20:50:52 +0100

    Grabar el evento en la base de datos

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
 const getEventos = (req, res = response) => {
   res.json({
@@ -7,14 +8,24 @@ const getEventos = (req, res = response) => {
   });
 };
 
-const crearEvento = (req, res = response) => {
-  // verificar que tenga el evento
-  console.log(req.body);
+const crearEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
+  try {
+    evento.user = req.uid;
 
-  res.json({
-    ok: true,
-    msg: 'crearEvento',
-  });
+    const eventoGuardado = await evento.save();
+
+    res.status(201).json({
+      ok: true,
+      evento: eventoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
 };
 
 const actualizarEvento = (req, res = response) => {
