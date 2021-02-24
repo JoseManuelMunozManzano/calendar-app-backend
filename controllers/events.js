804f6e86dfdd2e36eeb8db59cb9fac1c46b400bf
Neimerc <Neil_Mercury@yahoo.es>
2021-02-24 20:44:30 +0100

    Eliminar Eventos

--- controllers/events.js
+++ controllers/events.js
@@ -39,14 +39,14 @@ const actualizarEvento = async (req, res = response) => {
     if (!evento) {
       return res.status(404).json({
         ok: false,
-        msg: 'Evento no existe por ese id',
+        msg: 'Evento no existe para ese id',
       });
     }
 
     if (evento.user.toString() !== uid) {
       return res.status(401).json({
         ok: false,
-        msg: 'No tiene privilegio de editar este evento',
+        msg: 'No tiene privilegios para editar este evento',
       });
     }
 
@@ -77,14 +77,39 @@ const actualizarEvento = async (req, res = response) => {
   }
 };
 
-const eliminarEvento = (req, res = response) => {
-  const { id } = req.params;
+const eliminarEvento = async (req, res = response) => {
+  const eventoId = req.params.id;
+  const uid = req.uid;
 
-  res.json({
-    ok: true,
-    id,
-    msg: 'eliminarEvento',
-  });
+  try {
+    const evento = await Evento.findById(eventoId);
+
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Evento no existe para ese id',
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegios para eliminar este evento',
+      });
+    }
+
+    await Evento.findByIdAndDelete(eventoId, { useFindAndModify: false });
+
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
 };
 
 module.exports = {
