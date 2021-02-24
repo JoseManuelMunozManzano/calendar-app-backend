0163527f3bafd29c34f8e8cb45b82bd3f832172d
Neimerc <Neil_Mercury@yahoo.es>
2021-02-24 20:22:10 +0100

    Actualizar un Evento

--- routes/events.js
+++ routes/events.js
@@ -37,7 +37,16 @@ router.post(
 );
 
 // Actualizar evento
-router.put('/:id', actualizarEvento);
+router.put(
+  '/:id',
+  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos,
+  ],
+  actualizarEvento
+);
 
 // Borrar evento
 router.delete('/:id', eliminarEvento);
