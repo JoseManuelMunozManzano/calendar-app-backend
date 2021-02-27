78a517de672ba5cfd3315ff7fc2fc9871ab581ec
Neimerc <Neil_Mercury@yahoo.es>
2021-02-27 08:27:04 +0100

    Agregando la parte de React terminada

---
+++ public/precache-manifest.bc09a1f9fe956c8ac9888542e862873c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c18b79c168ba8b1bc68e5dd763ef3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4da9a5b0db7cf8c604c",
+    "url": "/static/css/2.b498485e.chunk.css"
+  },
+  {
+    "revision": "b9c3506102050497be66",
+    "url": "/static/css/main.339234c8.chunk.css"
+  },
+  {
+    "revision": "f4da9a5b0db7cf8c604c",
+    "url": "/static/js/2.b4147e68.chunk.js"
+  },
+  {
+    "revision": "ee0032eb04899a5c7b5da7e478f17b32",
+    "url": "/static/js/2.b4147e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c3506102050497be66",
+    "url": "/static/js/main.760b28f4.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
